ceb072dc6f8871ce28ac738d8165bc5f58b2a1bb
Tobias Gall <tobias.gall@mailbox.org>
2016-11-12 14:18:22 +0100

    choices im Konstruktor setzen sonst knallt es, wenn es die Tabelle nicht gibt

--- django/app/forms.py
+++ django/app/forms.py
@@ -4,10 +4,10 @@ from django import forms
 
 class SubscribeForm(forms.Form):
     interval = forms.DurationField(required=True)
-    categories = forms.MultipleChoiceField(choices=Category.objects.all().values_list("pk","name"), required=True)
+    categories = forms.MultipleChoiceField(required=True)
     email = forms.EmailField(required=True)
 
     def __init__(self, *args, **kwargs):
         self.category_choices = kwargs.pop('category_choices')
         super(SubscribeForm, self).__init__(*args, **kwargs)
-        self.categories.choices = self.category_choices
+        self.fields['categories'].choices = self.category_choices
