0de81337607b5c69b7a86580043e7010ab4bf431
Morris Jobke <hey@morrisjobke.de>
2016-11-20 19:59:52 +0100

    move domains to separate config
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- django/theaterwecker/settings.py
+++ django/theaterwecker/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
--- django/theaterwecker/settings_dev.py
+++ django/theaterwecker/settings_dev.py
@@ -3,3 +3,5 @@ SECRET_KEY = 'w2#@_!5tgn3i@83di_itgr-r1pm4#rw&bd7^t0%j*!x5ja7=-v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+ALLOWED_HOSTS = []
