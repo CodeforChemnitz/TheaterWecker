3acaa141d363e2769c5900651ac1adb095f23851
Morris Jobke <hey@morrisjobke.de>
2016-11-20 23:08:06 +0100

    move database setup into env files
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- django/theaterwecker/settings_dev.py
+++ django/theaterwecker/settings_dev.py
@@ -5,3 +5,13 @@ SECRET_KEY = 'w2#@_!5tgn3i@83di_itgr-r1pm4#rw&bd7^t0%j*!x5ja7=-v'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
