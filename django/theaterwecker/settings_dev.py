ac97cbd0214573c06a45e825997c447341e2214d
Morris Jobke <hey@morrisjobke.de>
2016-11-21 01:23:57 +0100

    add a 404 page
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- django/theaterwecker/settings_dev.py
+++ django/theaterwecker/settings_dev.py
@@ -1,3 +1,8 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'w2#@_!5tgn3i@83di_itgr-r1pm4#rw&bd7^t0%j*!x5ja7=-v'
 
