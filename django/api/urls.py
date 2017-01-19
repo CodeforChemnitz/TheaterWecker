9ce6a46d997b43d44dbebe748c8b30a55df6ec60
Tobias Gall <tobias.gall@mailbox.org>
2017-01-19 20:45:54 +0100

    remove slash from url

--- django/api/urls.py
+++ django/api/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^device/$', device, name="device"),
+    url(r'^device$', device, name="device"),
     url(r'^verify/(?P<key>[a-f0-9]{32})$', verify, name="verify"),
-    url(r'^subscribe/$', subscribe, name="subscribe"),
-    url(r'^categories/$', categories, name="categories"),
+    url(r'^subscribe$', subscribe, name="subscribe"),
+    url(r'^categories$', categories, name="categories"),
 ]
