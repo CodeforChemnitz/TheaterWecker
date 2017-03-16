2e886578b04a1139b7fd94eeabdcd105b9d3ac05
Ronny Hartenstein <ronny@rh-flow.de>
2017-03-16 22:25:26 +0100

    Upgrade auf RN 0.42

--- app/android/app/src/main/java/de/codeforchemnitz/theaterwecker/MainApplication.java
+++ app/android/app/src/main/java/de/codeforchemnitz/theaterwecker/MainApplication.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeOneSignalPackage(),
             new VectorIconsPackage(),
             new ReactNativeOneSignalPackage()
       );
