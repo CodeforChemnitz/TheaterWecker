1c287df56bfd1664af6961590778017ea7fb0785
Tobias Gall <tobias.gall@mailbox.org>
2017-04-27 21:03:30 +0200

    update scraper.py

--- scraper/app.py
+++ scraper/app.py
@@ -13,7 +13,8 @@ from bs4 import BeautifulSoup
 # Use German month names
 locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
 
-URL = "http://www.theater-chemnitz.de/spielplan/gesamtspielplan"
+URL = "http://www.theater-chemnitz.de/spielplan/repertoire"
+time_re = re.compile("(?P<hour>\d{2}):(?P<minutes>\d{2})(\s*Uhr\s*)")
 time_location_re = re.compile("(?P<hour>\d{2}):(?P<minutes>\d{2})(\s*Uhr\s*)(?P<location>[\w\s]*)")
 calendar_months = [
     '',
@@ -33,56 +34,52 @@ calendar_months = [
 
 
 def get_plays(year, month):
-    plan = requests.get(URL, params={
-        "month": calendar_months[month],
-        "year": year,
-        "tip": 1,
-    })
+    plan = requests.get("{}/{}/{}".format(URL, year, month))
     if plan.status_code != 200:
         logger.error('got non-200 return code while scraping', exc_info=True)
         return []
     soup = BeautifulSoup(plan.text.replace('&nbsp;', ' '), "lxml")
-    block_tops = soup.find_all("div", class_="block_top")
+    news_items = soup.find_all("div", class_="cc_news_item")
     plays = []
-    for block_top in block_tops:
-        date = block_top.find("div", class_="pr_box_data_01")
+    for block_top in news_items:
+        date = block_top.find("div", class_="cc_news_date")
         if date:
-            day = int(date.get_text())
-            infos = block_top.find_all("div", class_="pr_box_info")
-            for info in infos:
-                play = {
-                    "month": month,
-                    "day": day,
-                    "year": year
-                }
-                time_location_raw = info.find(class_="news_box_in_left_in_top")
-                m = time_location_re.match(time_location_raw.get_text())
-                play["hour"] = int(m.group("hour"))
-                play["minutes"] = int(m.group("minutes"))
-                play["location"] = m.group("location")
-                # Gastspiel, Premiere etc.
-                special_raw = info.find("span", class_="pr_red")
-                if special_raw:
-                    special = special_raw.get_text()
-                    if special in ["Gastspiel"]:  # Ausnahmen
-                        continue
-                category_raw = info.find(class_="pr_box_content_right_table_top")
-                if category_raw:
-                    category = category_raw.get_text()
-                    if category in ["Theaternahes Rahmenprogramm"]:
-                        category = "Sonstiges"
-                    play["category"] = category
-                title_raw = info.find(class_="mini_title_link_b")
-                if title_raw:
-                    play["title"] = title_raw.get_text()
-                else:
+            print(date)
+            print('\n\n')
+            day = int(date.find(class_="cc_day").get_text().strip('.'))
+            play = {
+                "month": month,
+                "day": day,
+                "year": year
+            }
+            time_raw = date.find(class_="cc_timeresp")
+            m = time_re.match(time_raw.get_text())
+            play["hour"] = int(m.group("hour"))
+            play["minutes"] = int(m.group("minutes"))
+            play["location"] = block_top.find(class_="cc_content").get_text()
+            # Gastspiel, Premiere etc.
+            special_raw = block_top.find(class_="cc_premiere")
+            if special_raw:
+                special = special_raw.get_text()
+                if special in ["Gastspiel"]:  # Ausnahmen
                     continue
-                desciption_raw = info.find(class_="news_box_descript")
-                if desciption_raw:
-                    play["description"] = desciption_raw.get_text()
-                tickets_raw = info.find("a", class_="karten")
-                play["tickets"] = tickets_raw
-                plays.append(play)
+            category_raw = date.find(class_="cc_type")
+            if category_raw:
+                category = category_raw.get_text()
+                if category in ["Theaternahes Rahmenprogramm"]:
+                    category = "Sonstiges"
+                play["category"] = category
+            title_raw = block_top.find("h2")
+            if title_raw:
+                play["title"] = title_raw.get_text()
+            else:
+                continue
+            desciption_raw = block_top.find("h3")
+            if desciption_raw:
+                play["description"] = desciption_raw.get_text()
+            tickets_raw = block_top.find("a", class_="cc_ticket")
+            play["tickets"] = tickets_raw
+            plays.append(play)
     if len(plays) == 0:
         logger.error('could not find a single play while scraping', exc_info=True)
     return plays
