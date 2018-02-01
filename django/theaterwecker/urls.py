110a4c08132082045be1c7518cd03498a24c34bf
Tobias Gall <tobias.gall@mailbox.org>
2018-02-01 22:21:25 +0100

    web-push initial

--- django/theaterwecker/urls.py
+++ django/theaterwecker/urls.py
@@ -17,11 +17,28 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.views import View
+from django.views.generic import RedirectView
+
+
+class OneSignalSDKView(View):
+
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(
+            request=request,
+            template='js/{}'.format(kwargs['path'].split('?')[0]),
+            content_type="application/javascript"
+        )
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'^api/', include('api.urls', namespace="api")),
+    url(r'^api/', include('api.urls', namespace="api")),
     url(r'^', include('app.urls', namespace="app")),
+    url(r'^manifest.json$', RedirectView.as_view(url='/static/manifest.json')),
+    url(r'^(?P<path>OneSignalSDK.*)$', OneSignalSDKView.as_view()),
 ]
 
 if settings.DEBUG:
